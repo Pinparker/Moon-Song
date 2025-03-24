d76302b4a34cf8dfd60f61d33008856af5c148d9
TomatoHB <binhuang98@live.com>
2025-03-24 14:01:01 +0800

    feat: 添加项目初始文件和资源，包括音频、图像、组件及配置文件

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/moon-song/",
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+      { find: "@assets", replacement: resolve(__dirname, "public/assets") },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
+      { find: "@common", replacement: resolve(__dirname, "src/common") },
+      { find: "@groups", replacement: resolve(__dirname, "src/groups") },
+    ],
+  },
+});
